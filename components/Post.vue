7a220ef7128cb87f7588b717aceef39846bdd496
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-12-11 01:12:14 +0900

    hoge

---
+++ components/Post.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nuxt-link to="/about">{{ item.name }}</nuxt-link>
+        <p>{{ item.content }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+export default({
+    props: {
+        item: {
+            type: Object,
+        }
+    }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <p>Here's a counter:</p>
     <div class="flex items-center justify-center">
       
-      <button @click="counter++" class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700">
+      <button @click="counter++" v-if="name === 'hoge'" class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700">
         +
       </button>
       <span class="mx-2">{{ counter }}</span>
@@ -11,19 +11,62 @@
         -
       </button>
     </div>
+    <div v-for="item in items" :key="item.id">
+        <post :item="item"/>
+    </div>
+    <div>
+        <button @click="flag = !flag">click</button>
+        <p v-if="flag">押した</p>
+        <p v-else>押してない</p>
+    </div>
+    <div>
+        <form>
+            <label>
+                test:
+                <input v-model="text" placeholder="入力ください" type="text">
+            </label>
+        </form>
+        <p>{{ text }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import Post from "~/components/Post.vue"
 
-export default defineComponent({
+export default ({
   layout: 'custom',
-  setup() {
-    const counter = ref(0)
-
+  components:{
+      post: Post
+  },
+  data() {
     return {
-      counter
+      counter: 0,
+      items: [
+          {
+              id: "1",
+              name: "huga1",
+              content: "This is pen."
+          },
+          {
+              id: "2",
+              name: "huga2",
+              content: "This is apple."
+          },
+          {
+              id: "3",
+              name: "huga3",
+              content: "This is car."
+          },
+          {
+              id: "4",
+              name: "huga4",
+              content: "This is cat."
+          },
+      ],
+      flag: false,
+      name: "hoge",
+      text: ""
     }
   }
 })
